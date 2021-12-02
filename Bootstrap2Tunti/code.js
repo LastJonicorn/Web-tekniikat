aa635a8bc0a93703bf7932a482cb8b33d6e631b8
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2021-12-02 10:33:46 +0200

    Bootstrap harjoitus

---
+++ Bootstrap2Tunti/code.js
@@ -0,0 +1,19 @@
+let alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+let alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  let wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    if(document.getElementById("firstname").value.length > 0){
+        alert('Form sent succesfully!', 'success')
+    } else{
+        alert('Please enter firstname!', 'danger')
+    }
+  })
+}
